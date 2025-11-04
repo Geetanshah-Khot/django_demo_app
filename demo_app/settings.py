a6066d849a09184f1081d51e04ef25172d793b23
Geetanshah-Khot <73351649+Geetanshah-Khot@users.noreply.github.com>
2025-11-04 22:37:55 +0530

    Update settings.py

--- demo_app/settings.py
+++ demo_app/settings.py
@@ -17,6 +17,34 @@ INSTALLED_APPS = [
     "core",
     "rest_framework",
 ]
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+ROOT_URLCONF = "demo_app.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "core" / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "demo_app.wsgi.application"
 SECRET_KEY = os.getenv("SECRET_KEY", "replace-with-a-secret-key")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
@@ -38,3 +66,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     Path(rest_framework.__file__).resolve().parent / "static",
 ]
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "Asia/Kolkata"
+USE_I18N = True
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
