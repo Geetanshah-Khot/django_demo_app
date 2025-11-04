f9da49c11b35fc96eea26db9634be6430ae71c58
Geetanshah-Khot <73351649+Geetanshah-Khot@users.noreply.github.com>
2025-11-04 19:29:59 +0530

    Update settings.py

--- demo_app/settings.py
+++ demo_app/settings.py
@@ -73,14 +73,12 @@ TIME_ZONE = "Asia/Kolkata"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
