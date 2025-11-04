721044b772fa537805c42f01e24312f5abc9cf0b
Geetanshah-Khot <73351649+Geetanshah-Khot@users.noreply.github.com>
2025-11-04 14:09:24 +0530

    Update settings.py

--- demo_app/settings.py
+++ demo_app/settings.py
@@ -53,9 +53,18 @@ TEMPLATES = [
 WSGI_APPLICATION = "demo_app.wsgi.application"
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
-}
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = []
 LANGUAGE_CODE = "en-us"
